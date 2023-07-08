c14183fb06ecc4c62d2daaab9af829ed35ddec9a
codeabuu <abdulngui718@gmail.com>
2023-07-08 15:13:37 -0700

    method 2 of alarm clock code

--- alarm2.py
+++ alarm2.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import time
 import datetime
+import pytz
 
 
 def set_alarm(time_set):
     while True:
-        current_time = datetime.datetime.now().strftime("%H:M:%S")
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
         if current_time == time_set:
             print("Time to wake up and grind!")
             break
@@ -13,12 +14,12 @@ def set_alarm(time_set):
             print("Time is: " + current_time)
             time.sleep(1)
             snooze = input("Press any alphabet key to snooze: ")
-            if snooze == "97 - 122":
+            if snooze.isalpha():
                 time_set = (datetime.datetime.now() + datetime.timedelta(minutes=1).strftime("%H:%M:%S"))
                 print("Alarm snoozed for 1 min.")
             else:
                 break
             time.sleep(1)
 
-time_set = input("Enter time respectively, Hours:Minutes:Seconds")
+time_set = input("Enter time in HH:M:S: ")
 set_alarm(time_set)
