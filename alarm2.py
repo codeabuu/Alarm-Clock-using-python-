9118f964d4c3e451ea0f43467f248f37e858d60e
codeabuu <abdulngui718@gmail.com>
2023-07-16 15:39:18 -0700

    update on alarm2

--- alarm2.py
+++ alarm2.py
@@ -5,10 +5,15 @@ import datetime
 
 def set_alarm(time_set):
     while True:
+        time_set = datetime.time()
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        time_left = (datetime.datetime.combine(datetime.date.today(), time_set) -
+                     datetime.datetime.combine(datetime.date.today(), datetime.datetime.strptime(current_time, "%H:%M:%S").time()))
+
         if current_time == time_set:
             print("Time to wake up and grind!")
             break
         else:
-            print(current_time)
+            print("Current time", current_time, end=" , ")
+            print("Time left: ", time_left)
             time.sleep(1)
